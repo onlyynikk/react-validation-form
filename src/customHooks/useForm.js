e32953a3c3b36722a79fd9fa1aed3486da30111a
Nikhilesh Jadhav <nikhileshj30@gmail.com>
2021-10-08 17:00:09 +0530

    first commit

--- src/customHooks/useForm.js
+++ src/customHooks/useForm.js
@@ -8,7 +8,7 @@ export default function useForm(callback, validate) {
     password2: "",
   });
   const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  //   const [isSubmitting, setIsSubmitting] = useState(false);
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -22,18 +22,15 @@ export default function useForm(callback, validate) {
     e.preventDefault();
 
     setErrors(validate(values));
-    setIsSubmitting(true);
+    // setIsSubmitting(true);
   }
 
-  function conditionToSumbit() {
-    if (Object.keys(errors).length === 0 && isSubmitting) {
-      callback();
-    }
-  }
-
-  useEffect(() => {
-    conditionToSumbit();
-  }, [errors]);
+  //   useEffect(() => {
+  //     if (Object.keys(errors).length === 0 && isSubmitting) {
+  //       callback();
+  //     }
+  //     // eslint-disable-next-line react-hooks/exhaustive-deps
+  //   }, [errors]);
 
   return { handleChange, handleSubmit, values, errors };
 }
